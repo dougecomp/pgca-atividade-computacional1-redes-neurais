e0c4e08e493ff47786594b34c8a449c6bfcbb093
Douglas Silva <douglaseusilva@gmail.com>
2016-10-20 23:27:39 -0300

    Submissão inicial do projeto.

---
+++ src/exemplos/ExercicioComputacional1.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package exemplos;
+
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import listeners.NeuralNetworkValidationListener;
+import org.neuroph.core.NeuralNetwork;
+import org.neuroph.core.data.DataSet;
+import org.neuroph.nnet.MultiLayerPerceptron;
+import org.neuroph.util.TransferFunctionType;
+
+/**
+ *
+ * @author douglas
+ */
+public class ExercicioComputacional1 {
+
+    public static DataSet getDataSet(String filename, String separator) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream("60percTrainingSet.txt")));
+                                    
+        double[] x = new double[1];
+        double[] y = new double[1];
+
+        DataSet trainingSet = new DataSet(1, 1);
+        // Ler o conjunto de dados de um arquivo
+        
+        String line = reader.readLine();
+        while(line!=null){
+            String[] partes = line.split(separator);
+            
+            x[0] = Double.parseDouble(partes[0]);
+            
+            y[0] = Double.parseDouble(partes[1]);
+
+            trainingSet.addRow(x, y);
+            //file.insert(file.createRecord(id, lat, lgt, title));
+
+            line = reader.readLine();
+        }   
+        reader.close();
+        
+        return trainingSet;
+    }
+    
+    public static void createNnet() {
+        
+    }
+    
+    public static void trainNnet() {
+        
+    }
+    
+    public static void validateNnet() {
+        
+    }
+    
+    public static void testNnet() {
+        
+    }
+    
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) throws IOException {
+        // TODO code application logic here
+        int qtdInputNeurons = 1;
+        int qtdHiddenNeuros = 5;
+        int qtdOutputNeurons = 1;
+        
+        NeuralNetwork mlp = new MultiLayerPerceptron(TransferFunctionType.SIGMOID, qtdInputNeurons, qtdHiddenNeuros, qtdOutputNeurons);
+        mlp.addListener(new NeuralNetworkValidationListener());
+        
+        
+    }
+    
+}
---
+++ src/listeners/NeuralNetworkValidationListener.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package listeners;
+
+import org.neuroph.core.events.NeuralNetworkEvent;
+import org.neuroph.core.events.NeuralNetworkEventListener;
+
+/**
+ *
+ * @author douglas
+ */
+public class NeuralNetworkValidationListener implements NeuralNetworkEventListener {
+
+    @Override
+    public void handleNeuralNetworkEvent(NeuralNetworkEvent nne) {
+        //throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
+    }
+    
+}
